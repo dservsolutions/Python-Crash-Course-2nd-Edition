8bb3e69ce473b0867760cff8489854ac793a6bf3
David Pedroso <ppdavisito89@gmail.com>
2023-10-14 12:53:19 -0400

    Exercises

--- Try it Yourself(Page 25).py
+++ Try it Yourself(Page 25).py
@@ -1,3 +1,13 @@
 # 2.3 Personal Message
 name = input('Please enter your name: ')
 print(f'Hello {name}, would you like to learn some Python Today ?')
+
+# 2.4 Name Cases
+print(name.lower(), name.upper(), name.title())
+
+#2.5 Famous Quote
+quote = 'A person who never made a mistake never tried anything new.'
+print(f'Albert Einstein once said, "{quote}"')
+
+# 2.6 Famous Quote #2
+famous_person = 'Albert Einstein'
