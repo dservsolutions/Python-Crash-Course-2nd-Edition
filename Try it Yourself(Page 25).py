fde93e05bd0841422c65a3c7dd9ccf39d1ca90ea
David Pedroso <ppdavisito89@gmail.com>
2023-10-14 14:14:13 -0400

    Complete

--- Try it Yourself(Page 25).py
+++ Try it Yourself(Page 25).py
@@ -5,9 +5,16 @@ print(f'Hello {name}, would you like to learn some Python Today ?')
 # 2.4 Name Cases
 print(name.lower(), name.upper(), name.title())
 
-#2.5 Famous Quote
+# 2.5 Famous Quote
 quote = 'A person who never made a mistake never tried anything new.'
 print(f'Albert Einstein once said, "{quote}"')
 
 # 2.6 Famous Quote #2
-famous_person = 'Albert Einstein'
+famous_person = 'Benjamin Franklin'
+quote='Three can keep a secret, if two of them are dead.'
+print(f'{famous_person} once said, "{quote}"')
+
+# 2.7 Stripping names
+name = ' Jhon '
+print(name.strip(), name.rstrip(), name.lstrip())
+print('\tDavid, \nJuan, \nPedro')
