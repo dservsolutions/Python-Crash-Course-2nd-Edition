f3f5b3a2f5f2d42b930987fd949249274c35499f
David Pedroso <ppdavisito89@gmail.com>
2023-10-19 11:59:02 -0400

    Unit 4 Working with List

---
+++ Unit 4 Working with List/Try it Yourself(Page 56).py
@@ -0,0 +1,15 @@
+# 4.1 Pizzas
+pizzas = ['pepperoni', 'napolitana', 'hawaiian', 'margherita', 'veggie']
+for pizza in pizzas:
+    # print(pizza) 
+    # (a)
+    print(f'I like {pizza.title()} pizza.')
+# (b)
+print('I really like pizzas..')
+
+# 4.2 Animals
+animals = ['jaguar', 'leopard', 'alligator', 'cocodrile', 'turtle', 'tortoise']
+for animal in animals:
+    # print(animal)
+    print(f'A {animal.title()} would make a great pet ')
+print('All this animals have similar characteristics ')
---
+++ Unit 4 Working with List/workin_with_list.py
@@ -0,0 +1,16 @@
+# Unit  Working with List
+# Looping through an Entire List
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print (magician)
+
+# Doing more work Withing a for Loop
+for magician in magicians:
+    print(f'{magician.title()}, that was a great trick!')
+    print(f"I can't wait to see your next trick!,{magician.title()}.\n")
+
+# Doing something After a for Loop
+print("Thank you, everyone. That was a great magic show! ")
+
+# Avoiding Indentation Errors
+
