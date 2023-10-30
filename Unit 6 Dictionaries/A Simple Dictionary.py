2aff57f5a29bce77f62ad0b1bb86051db7bbcdaa
David Pedroso <ppdavisito89@gmail.com>
2023-10-30 11:35:19 -0400

    Unit 6 Dictionaries

---
+++ Unit 6 Dictionaries/A Simple Dictionary.py
@@ -0,0 +1,11 @@
+alien_0 = {
+    'color': 'green',
+    'points': '5'
+}
+
+print(alien_0['color'])
+
+
+# Working with Dictionaries
+new_point = alien_0['points']
+print(f"You just earn: {new_point} points!")
