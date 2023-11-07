e18fa42897c6eb3472733aed25c408c699b0e7b0
David Pedroso <ppdavisito89@gmail.com>
2023-11-07 11:35:24 -0500

    Unit 7 User input and while loops

---
+++ Unit 7 User Input and While Loops/Try it Yourlsef(Page 117).py
@@ -0,0 +1,20 @@
+# 7.1 Rental Car
+# car = input("What kind of car would you like rent ? ")
+# print(f"Let me see if I can find you a {car}")
+
+# 7.2 Restaurant Seating
+# dinner_group = int(input("How many people are in the group ?"))
+
+# if dinner_group > 8:
+#     print("They will have to wait for a table. ")
+# else:
+#     print("Welcome, the table is ready.")
+
+
+# 7.3 Multiple of Ten
+number = int(input("Enter a number please: "))
+
+if number % 10 == 0:
+    print("Is divisible")
+else:
+    print("Isn't divisible")
---
+++ Unit 7 User Input and While Loops/input_functions.py
@@ -0,0 +1,28 @@
+# Input Examples
+# message = input("Tell me something and I will repeat it back to you: ")
+# print(message)
+
+# Writing Clear Prompts
+# name = input("Please enter your name: ")
+# print(f"\n Hello, {name}")
+
+# Using int() to Accept Numerical Input
+# age = input("How old are you?")
+# print(age) 
+
+# Rollercoaster
+# height = input("How tall are you, in inches?")
+# height = int(height)
+
+# if height >= 48:
+#     print("\n You're tall enough to ride ")
+# else:
+#     print("\n You'll be able to ride when you're a little older. ")
+
+# Even or Odd Example
+number = int(input("Enter a number and I'll tell you if is even or odd: "))
+
+if number % 2 == 0:
+    print(f"The number {number} is Even")
+else:
+    print(f"The number {number} is Odd")
