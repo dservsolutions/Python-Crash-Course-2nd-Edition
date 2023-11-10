9352da2c2e41a88dd316e60f9591575d44512dca
David Pedroso <ppdavisito89@gmail.com>
2023-11-10 17:17:58 -0500

    Unit 8 Functions

---
+++ Unit 8 Functions/Try it Yourself(Page 131).py
@@ -0,0 +1,13 @@
+# 8.1 Message
+
+def display_message():
+    print("We are learning in this chapter how to work with functions.")
+
+display_message()
+
+# 8.2 Favorite Book
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}")
+
+favorite_book("Harry Potter")
