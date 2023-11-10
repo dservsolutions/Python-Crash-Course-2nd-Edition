4263f5fa01b87e49b76d2007e680cd0abf09a6fe
David Pedroso <ppdavisito89@gmail.com>
2023-11-10 18:53:24 -0500

    Unit 8 Functions

--- Unit 8 Functions/Try it Yourself(Page 131).py
+++ Unit 8 Functions/Try it Yourself(Page 131).py
@@ -11,3 +11,18 @@ def favorite_book(title):
     print(f"One of my favorite books is {title.title()}")
 
 favorite_book("Harry Potter")
+
+# 8.3 T-Shirt
+def make_shirt(size, tshirt_quotes):
+    print(f"\nSize of the T-Shirt: {size.title()}")
+    print(f"Quote to print: {tshirt_quotes.title()}")
+
+make_shirt('M', 'Hello World') 
+make_shirt(size='S', tshirt_quotes='Have a good one.')
+
+# 8.4 Large Shirts
+def make_largue_tshirt(large_tshirt_quote, size="L"):
+    print(f"\nSize of the T-Shirt: {size.title()}")
+    print(f"Quote to print: {large_tshirt_quote}")
+
+make_largue_tshirt(large_tshirt_quote="Welcome to USA")
--- Unit 8 Functions/defining_a_function.py
+++ Unit 8 Functions/defining_a_function.py
@@ -6,7 +6,17 @@
 # greet_user()
 
 # Passing information to a function. 
-def greet_user(username):
-    print(f"Hello {username.title()}")
+# def greet_user(username):
+#     print(f"Hello {username.title()}")
 
-greet_user('david') 
+# greet_user('david') 
+
+# Passing Arguments
+# Positional Arguments
+def describe_pet(animal_type, pet_name):
+    """Display informatin about a pet."""
+    print(f"\nI have a {animal_type}. ")
+    print(f"My {animal_type}'s name is {pet_name.title()}")
+
+describe_pet("dog", "lila")
+describe_pet(animal_type='cat', pet_name='cloe')
