7f258e01ccdf760750193a5f0fba89717e664178
David Pedroso <ppdavisito89@gmail.com>
2023-11-11 17:11:57 -0500

    Unit 8 Functions

--- Unit 8 Functions/Try it Yourself(Page 131).py
+++ Unit 8 Functions/Try it Yourself(Page 131).py
@@ -26,3 +26,12 @@ def make_largue_tshirt(large_tshirt_quote, size="L"):
     print(f"Quote to print: {large_tshirt_quote}")
 
 make_largue_tshirt(large_tshirt_quote="Welcome to USA")
+
+# 8.5 Cities 
+
+def describe_city(city, country='USA'):
+    print(f"{city.title()} is in {country}")
+
+describe_city('Talahasse')
+describe_city('Tampa')
+describe_city('Havana', country='Cuba')
--- Unit 8 Functions/defining_a_function.py
+++ Unit 8 Functions/defining_a_function.py
@@ -13,10 +13,43 @@
 
 # Passing Arguments
 # Positional Arguments
-def describe_pet(animal_type, pet_name):
-    """Display informatin about a pet."""
-    print(f"\nI have a {animal_type}. ")
-    print(f"My {animal_type}'s name is {pet_name.title()}")
+# def describe_pet(animal_type, pet_name):
+#     """Display informatin about a pet."""
+#     print(f"\nI have a {animal_type}. ")
+#     print(f"My {animal_type}'s name is {pet_name.title()}")
 
-describe_pet("dog", "lila")
-describe_pet(animal_type='cat', pet_name='cloe')
+# describe_pet("dog", "lila")
+# describe_pet(animal_type='cat', pet_name='cloe')
+
+# Returning a Simple Value
+# def get_formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = f"{first_name.title()} {last_name.title()}"
+#     return full_name.title()
+
+# musician = get_formatted_name('jimi', 'hendrix')
+# print(musician)
+
+
+# Making a Argument Optional
+# def get_formatted_name(first_name, last_name, middle_name=''):
+#     """Return a full name, neatly formatted. """
+#     if middle_name:
+#         full_name = f"{first_name} {middle_name} {last_name}"
+#     else:
+#         full_name = f"{first_name} {last_name}"
+#         return full_name.title()
+    
+# actor = get_formatted_name("Jin", "carrey", "gonzalez")
+# print(actor)
+# men = get_formatted_name("Jimmy", "Fallen")
+# print(men)
+
+# Returning a Dictionary
+def build_person(first_name, last_name):
+    """Return a dictionary information about a person."""
+    person = {'first': first_name, 'last': last_name} 
+    return person
+
+musician = build_person(first_name='Jymmy', last_name='Fallen')
+print(musician)
