777d84860bef686d06d1a71627af666722927a44
David Pedroso <ppdavisito89@gmail.com>
2023-12-02 12:26:35 -0500

    Unit 9 Classes

---
+++ Unit 9 Classes/creating_a_class.py
@@ -0,0 +1,16 @@
+# Creating and Using a Class
+class Dog:
+    """A simple attemp to model a dog."""
+    def __init__(self, name, age) -> None:
+        """Initialize name and age attributes"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """Simulate rolling over in response to a command"""
+        print(f"{self.name} rolled over! ")
+
