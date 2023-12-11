731147142d5d27dfd224754e0d8e4b55a85f2eaf
David Pedroso <ppdavisito89@gmail.com>
2023-12-11 08:35:45 -0500

    Unit 9 Classes

---
+++ Unit 9 Classes/importing_classes.py
@@ -0,0 +1,5 @@
+# Importing a Single Class
+from car import Car
+
+ford_fusion = Car('Ford', 'Fusion', 2022)
+
---
+++ Unit 9 Classes/students.py
@@ -0,0 +1,16 @@
+class Students:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    # Method student info
+    def students_inf(self):
+        print(f"Full Name: {self.first_name.title()} {self.last_name.title()}")
+        print(f"Age: {self.age}")
+        
+# Making a instance
+std_1 = Students('David', 'Pedroso', 33)
+
+
+std_1.students_inf()
