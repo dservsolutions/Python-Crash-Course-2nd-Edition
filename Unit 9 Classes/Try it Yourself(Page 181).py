85b97a97e66b91c794d06ab182a2e8019ec12109
David Pedroso <ppdavisito89@gmail.com>
2023-12-15 18:44:18 -0500

    Unit 9 Classes

--- Unit 9 Classes/Try it Yourself(Page 181).py
+++ Unit 9 Classes/Try it Yourself(Page 181).py
@@ -14,11 +14,27 @@ class Dice:
         
 
 # Making instances
-dice_6 = Dice(sides= 6)
-dice_6.roll_die()
+# dice_6 = Dice(sides= 6)
+# dice_6.roll_die()
 
-dice_10 = Dice(sides= 10)
-dice_10.roll_die()
+# dice_10 = Dice(sides= 10)
+# dice_10.roll_die()
 
-dice_20 = Dice(sides=20)
-dice_20.roll_die()
+# dice_20 = Dice(sides=20)
+# dice_20.roll_die()
+
+
+# 9.14 Lottery
+numbers = (1, 12, 14, 22, 33, 44, 51, 61, 88, 102, 'a', 'b', 'c', 'd', 'e', 'f')
+random_items = random.sample(numbers, 4)
+# print(f"None ticket match with these charapters: ")
+# for items in random_items:
+#     print(items)
+
+# 9.15 Lottery Analysis:
+my_ticket = (12, 1, 33, 'a')
+stop = False
+while stop:
+    for items in random_items:
+        print(items)
+       
